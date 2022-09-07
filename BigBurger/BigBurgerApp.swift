b88a9f4e4fdf34bf17d2d6942a09ae778d250da9
Maxime <imax-my@live.fr>
2022-09-07 14:04:36 +0200

    Initial Commit

---
+++ BigBurger/BigBurgerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BigBurgerApp.swift
+//  BigBurger
+//
+//  Created by Maxime on 07/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct BigBurgerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
